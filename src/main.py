fc74c7c76f0e9c22f3a946aff35e7ea7cdcc591e
Paul Hamshere <p.w.hamshere@gmail.com>
2022-11-03 20:25:58 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import time
 import device, midi
 
 usbd = device.get()
-#a = midi.AudioUSB()
+a = midi.AudioUSB()
 m = midi.MidiUSB()
 
-#usbd.add_interface(a)
+usbd.add_interface(a)
 usbd.add_interface(m)
 usbd.reenumerate()
 
@@ -14,22 +14,3 @@ time.sleep(3)  # TODO: provide a way to find out at runtime if an interface is a
 
 m.send_midi()
 
-"""
-import time
-import device, hid
-
-ud = device.get()
-
-m = hid.MouseInterface()
-ud.add_interface(m)
-ud.reenumerate()
-
-time.sleep(2)  # TODO: provide a way to find out at runtime if an interface is active
-
-m.move_by(-100, 0)
-time.sleep(0.25)
-
-m.click_right(True)
-time.sleep(0.25)
-m.click_right(False)
-"""
